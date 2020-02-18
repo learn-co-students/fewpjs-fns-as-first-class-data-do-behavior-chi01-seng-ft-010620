30d48eff0ac1436cee9a4dd9b7f4aa08cc93c6c9
learn <github email address>
2020-02-18 10:09:48 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let timeSplit = timeString.split(":")
+  let timeHourString = timeSplit[0]
+  let timeHour = parseInt(timeHourString)
+
+  if (timeHour < 12){
+    return "Good morning"
+  }
+  else if (timeHour > 12 && timeHour < 5) {
+  return "Good Afternoon"}
+
+  else 
+  { return "Good Evening"}
+}
 /* Write your implementation of displayMessage() */
